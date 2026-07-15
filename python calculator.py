22b30fdbc4bc8a8552e5dcb9ea0b154d00f6507e
kritikraj16 <rajkritik1604@gmail.com>
2026-07-16 01:36:54 +0530

    Add files via upload

---
+++ python calculator.py
@@ -0,0 +1,12 @@
+first = int(input("enter first number: "))
+second = int(input("enter second number: "))
+
+sum = first + second
+difference = first - second
+product = first * second
+division = first / second
+
+print(sum)
+print(difference)
+print(product)
+print(division)
